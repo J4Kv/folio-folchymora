6a129d9306075dcfdfcb0543fb7ddea4603f1c5c
J4kv <thej4kv@gmail.com>
2025-05-05 18:58:07 -0400

    Integrado react-helmet con verificación Google

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
 import Hero from "../components/Hero";
+import { Helmet } from "react-helmet";
 
 function Home() {
   return (
     <div>
+      <Helmet>
+        <title>Inicio | Folch & Mora Abogados</title>
+        <meta
+          name="google-site-verification"
+          content="ow3ZnspbBx68zelQLpsenjxPuutbDAvWee6iOVZ_A0g" 
+        />
+        <meta name="description" content="Defendemos tus derechos con excelencia. Estudio Jurídico Folch & Mora, especialistas en derecho de familia, laboral y penal en la region de Valparaiso" />
+      </Helmet>
       <Hero />
     </div>
   );
